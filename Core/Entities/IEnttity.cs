9f7420c94f5a248097af04a61a21edbed63bd5a7
irem dilik <iremdilik@gmail.com>
2021-08-22 16:00:43 +0300

    9.ders sonu

--- Core/Entities/IEnttity.cs
+++ Core/Entities/IEnttity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //IEntity'i bu bir veri tablosudur demek için
     //IEntity implement eden class bir veritabanı tablosudur
--- Entities/Concrete/Category.cs
+++ Entities/Concrete/Category.cs
@@ -1,4 +1,4 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,6 +11,5 @@ namespace Entities.Concrete
     {
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
-
     }
 }
