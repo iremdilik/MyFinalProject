ec0b846fe6099e91eda519e173f18ac7ad0e8d0f
irem dilik <iremdilik@gmail.com>
2021-09-05 21:22:12 +0300

    14.gün

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
         public static string CategoryLimitExceded = "Kategori limiti aşıldı, yeni ürün eklenemiyor.";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserRegistered = "kullanıcı kayıt oldu.";
+        public static string UserNotFound = "kullanıcı bulunamadı.";
+        public static string PasswordError = "şifre hatası.";
+        public static string SuccessfulLogin = "başarılı giriş";
+        public static string UserAlreadyExists = "kullanıcı mevcut";
+        public static string AccessTokenCreated = "token oluşturuldu";
     }
 }
